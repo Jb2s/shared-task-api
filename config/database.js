63a1602371323f66b366f1d3780f231070935fc1
Jb2s <toukamjeanbernard@gmail.com>
2025-02-04 13:05:01 +0100

    v0.1.1

--- config/database.js
+++ config/database.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_USER,      
   process.env.DB_PASS,  
   {
-      host: process.env.DB_HOST,    
-      dialect: process.env.DB_DIALECT 
+    host: process.env.DB_HOST,    
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false // This disables strict SSL checks
+      }
+    }
   }
 );
 
